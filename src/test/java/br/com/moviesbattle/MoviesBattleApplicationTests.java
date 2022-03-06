bcaa9e8b59f1069b4fa0e20712eb720915d2070c
muriloalvesdev <murilo.alves@mercadolivre.com>
2022-03-06 19:22:09 -0300

    add auto config for in tests

--- src/test/java/br/com/moviesbattle/MoviesBattleApplicationTests.java
+++ src/test/java/br/com/moviesbattle/MoviesBattleApplicationTests.java
@@ -1,13 +1,15 @@
 package br.com.moviesbattle;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@EnableAutoConfiguration
 class MoviesBattleApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
