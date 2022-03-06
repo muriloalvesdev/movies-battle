066412e91f4e3a38ea01a7a9983df8f542a1be26
muriloalvesdev <murilo.alves@mercadolivre.com>
2022-03-06 19:19:44 -0300

    add enable caching

--- src/main/java/br/com/moviesbattle/MoviesBattleApplication.java
+++ src/main/java/br/com/moviesbattle/MoviesBattleApplication.java
@@ -2,12 +2,14 @@ package br.com.moviesbattle;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
+@EnableCaching
 @SpringBootApplication
 public class MoviesBattleApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MoviesBattleApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(MoviesBattleApplication.class, args);
+    }
 
 }
