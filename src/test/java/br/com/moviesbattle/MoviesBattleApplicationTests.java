af4c350dfba4cbf0c83db5e8d98032f3b53aba3e
muriloalvesdev <murilo.alves@mercadolivre.com>
2022-03-06 20:05:24 -0300

    add annotation

--- src/test/java/br/com/moviesbattle/MoviesBattleApplicationTests.java
+++ src/test/java/br/com/moviesbattle/MoviesBattleApplicationTests.java
@@ -3,7 +3,9 @@ package br.com.moviesbattle;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
+@ActiveProfiles("test")
 @SpringBootTest
 @EnableAutoConfiguration
 class MoviesBattleApplicationTests {
